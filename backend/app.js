555f8c616ec73bc276a8c2da53fc67957cba64cf
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2022-07-31 17:38:25 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,12 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path'); //pour les images je crois qu'il ne me reste plus que ça
 const userRoutes = require('./routes/user');
+const stuffRoutes = require('./routes/stuff');
 
-mongoose.connect(process.env.MONGO_PSWD, 
+mongoose.connect(process.env.MONGO_USER, //connexion a mongoDB avec dotenv
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
@@ -11,7 +14,7 @@ mongoose.connect(process.env.MONGO_PSWD,
 
 app.use(express.json());
 
-app.use((req, res, next) => {
+app.use((req, res, next) => { // header pour faire le lien entre le 3000 et le 4200 erreur CORS
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -19,4 +22,6 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/auth', userRoutes);
+app.use('/api/sauces', stuffRoutes);
+
 module.exports = app;
