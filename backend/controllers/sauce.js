245fd3ef67aac119df41228f2b21332a96ee1625
Benjamin Legeai <56591231+sayword@users.noreply.github.com>
2022-08-01 12:55:26 +0200

    Add files via upload

---
+++ backend/controllers/sauce.js
@@ -0,0 +1,76 @@
+const Sauce = require('../models/sauce');
+const fs = require('fs');
+
+//créer une sauce
+const createSauce = (req, res, next) => {
+  const sauceObject = JSON.parse(req.body.sauce);
+  delete sauceObject._id;
+  const sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Sauce enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+//montre la sauce selectionnée
+const getOneSauce = (req, res, next) => {
+  Sauce.findOne({
+    _id: req.params.id
+  }).then(
+    (sauce) => {
+      res.status(200).json(sauce);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+//modifie la sauce
+const modifySauce = (req, res, next) => {
+  console.log(req.body, req.file);
+  const sauceObject = req.file ?
+    {
+      ...JSON.parse(req.body.sauce),
+      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : { ...req.body };
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !' }))
+    .catch(error => res.status(400).json({ error }));
+};
+
+//Supprime la sauce en question
+const deleteSauce = (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({ _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+          .catch(error => res.status(400).json({ error }));
+      });
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+//montre toutes les sauces
+const getAllStuff = (req, res, next) => {
+  Sauce.find().then(
+    (sauces) => {
+      res.status(200).json(sauces);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    } 
+  );
+};
+
+module.exports={createSauce, getOneSauce, modifySauce, deleteSauce, getAllStuff};
